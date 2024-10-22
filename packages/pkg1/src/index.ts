76e3636629b89bb8b909d5f31f9ee020838431c5
301830 <jijm@bosc.cn>
2024-10-22 15:26:50 +0800

    feat: 添加 eslint 和 lint-staged

--- packages/pkg1/src/index.ts
+++ packages/pkg1/src/index.ts
@@ -3,7 +3,6 @@ import pkg2 from '@ivokc/monorepo2';
 function fun2() {
   pkg2();
   console.log('I am package 1');
-  console.log('hello world 1');
+  console.log('hello world 1')
 }
-
 export default fun2;
